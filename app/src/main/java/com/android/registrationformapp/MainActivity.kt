db0abddc6730c61772cd0c428cffddc3f7b3ae73
SnehashishRout <75388787+SnehashishRout@users.noreply.github.com>
2022-01-22 19:53:26 +0530

    Update MainActivity.kt

--- app/src/main/java/com/android/registrationformapp/MainActivity.kt
+++ app/src/main/java/com/android/registrationformapp/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompat() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
